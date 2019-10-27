396f395e8e5f6d4289268ca9660b3f23bc52d36d
Guilherme Welter <guilherme.a.welter@gmail.com>
2019-10-27 08:43:02 -0300

    Finalizando frontend dashboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
@@ -12,6 +13,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333, () => console.log("running on 3333"));
